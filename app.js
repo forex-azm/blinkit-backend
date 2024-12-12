1c67d60710e2f4741ea26b692043cdd5b67d4e5c
forex-azm <134093138+forex-azm@users.noreply.github.com>
2024-12-12 13:27:06 +0530

    host updated 0.0.0.0

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const start = async () => {
   });
   await registerRoutes(app);
   await buildAdminRouter(app);
-  app.listen({ port: PORT }, (err, addr) => {
+  app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
     if (err) {
       console.log(err);
     } else {
